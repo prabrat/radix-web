b9e1615de55995383e03a0eef5ce975beeedbdd9
Prabhat Jain <nishtimishti@gmail.com>
2024-12-27 22:16:36 -0500

    Initial

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,6 @@ import React from 'react'
 import "../styles/footer.css"
 
 import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function footer() {
   return (

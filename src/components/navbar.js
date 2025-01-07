f3a7e889ab56ec7bf220d40c9e1c0ec7a0c05952
Prabhat Jain <nishtimishti@gmail.com>
2025-01-06 20:35:26 -0500

    Updated Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/navbar.css";
 
 // Import Images
@@ -8,6 +8,8 @@ import MenuIcon from '@mui/icons-material/Reorder';
 
 function NavBar() {
     const [openLinks, setOpenLinks] = useState(false);
+    const location = useLocation();
+
     const toggleNavbar = () => {
         setOpenLinks(!openLinks);
     };
@@ -19,19 +21,19 @@ function NavBar() {
                     <img src={FullLogo} alt="Radix Childcare" />
                 </Link>
                 <div className="hiddenLinks">
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About Us</Link>
-                    <Link to="/credentials">Credentials</Link>
-                    <Link to="/forms">Forms</Link>
-                    <Link to="/contact">Contact Us</Link>
+                    <Link to="/" className={location.pathname === "/" ? "active" : ""}>Home</Link>
+                    <Link to="/about" className={location.pathname === "/about" ? "active" : ""}>About Us</Link>
+                    <Link to="/credentials" className={location.pathname === "/credentials" ? "active" : ""}>Credentials</Link>
+                    <Link to="/forms" className={location.pathname === "/forms" ? "active" : ""}>Forms</Link>
+                    <Link to="/contact" className={location.pathname === "/contact" ? "active" : ""}>Contact Us</Link>
                 </div>
             </div>
             <div className="rightSide">
-                <Link to="/">Home</Link>
-                <Link to="/about">About Us</Link>
-                <Link to="/credentials">Credentials</Link>
-                <Link to="/forms">Forms</Link>
-                <Link to="/contact">Contact Us</Link>
+                <Link to="/" className={location.pathname === "/" ? "active" : ""}>Home</Link>
+                <Link to="/about" className={location.pathname === "/about" ? "active" : ""}>About Us</Link>
+                <Link to="/credentials" className={location.pathname === "/credentials" ? "active" : ""}>Credentials</Link>
+                <Link to="/forms" className={location.pathname === "/forms" ? "active" : ""}>Forms</Link>
+                <Link to="/contact" className={location.pathname === "/contact" ? "active" : ""}>Contact Us</Link>
                 <button onClick={toggleNavbar}>
                     <MenuIcon />
                 </button>
